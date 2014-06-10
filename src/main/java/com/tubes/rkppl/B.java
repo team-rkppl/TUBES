c149be8423f6ac7bf96f9a86d9fe076d6f2c1600
danisofyan <danixsofyan@gmail.com>
2014-06-10 21:14:18 +0700

    Add class B, MulAndDivInterface, PowInterface

---
+++ src/main/java/com/tubes/rkppl/B.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tubes.rkppl;
+
+/**
+ *
+ * @author danisofyan
+ */
+public class B implements MulAndDivInterface{
+    
+    public B(){
+        
+    }
+    
+    public String toString(){
+        return "B";
+    }
+
+    @Override
+    public double mul(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double div(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/com/tubes/rkppl/MulAndDivInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tubes.rkppl;
+
+/**
+ *
+ * @author danisofyan
+ */
+public interface MulAndDivInterface {
+    
+    public double mul(double n1,double n2 );
+    
+    public double div(double n1, double n2);
+    
+}
